e773e461c5e593e643d28aacec73dfd117280e0b
Craig <CraigKapan@gmail.com>
2021-07-10 11:20:38 +0200

    My first commit

---
+++ 05-Guess-My-Number/starter/script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+// PROJECT #1: Guess My Number! //
+
+console.log(document.querySelector(".message").textContent);
+document.querySelector(".message").textContent = "🎉 Correct Number!";
+console.log(document.querySelector(".message").textContent);
